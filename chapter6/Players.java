4556d570eec8c00fab30ebf5251bd95a4e80b344
Ayanle Yusuf <33944195+Ayanle99@users.noreply.github.com>
2021-05-23 22:34:29 -0500

    Update Players.java

--- chapter6/Players.java
+++ chapter6/Players.java
@@ -2,6 +2,33 @@ import testPack.Coin;
 
 import java.util.Scanner;
 
+
+public class Heads_Or_Tails_game {
+
+    /**
+     *  This game is meant for two or more players. In this game, the players take turns flipping a
+     *  coin. Before the coin is flipped, players should guess if the coin will land headsup or tailsup.
+     *  If player guesses correctly, then that player is awarded a point.
+     *  If the player guesses incorrectly, the player loses a point.
+     *  The first player to score five points is the winner.
+     *
+     *  Write a program that simulates the game being played by two players. Use the Coin class
+     *  that you wrote as an assignment in Chapter 5 ( Programming Challenge 20) to simulate the
+     *  coin. Write a Player class to simulate the players.
+     *
+     */
+
+    public static void main(String[] args) {
+
+
+        Players players = new Players();
+        players.startGame();
+
+    }
+
+}
+
+
 public class Players {
 
     static Coin coin = new Coin();
@@ -92,5 +119,37 @@ public class Players {
     }
 
 
+
+}
+
+import java.util.Random;
+
+public class Coin {
+
+    private String sideUp;
+
+    public Coin() {
+        toss();
+    }
+
+    public void toss() {
+
+        Random rand = new Random();
+        int n = rand.nextInt(2);
+
+        if (n == 0)
+            sideUp = "heads";
+        else
+            sideUp = "tails";
+    }
+
+    public String getSideUp() {
+        return sideUp;
+    }
+
+    public void equals(String str) {
+
+        str = getSideUp();
+    }
 
 }
